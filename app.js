f34baf15467e9a164bb295d54923f21b27a5efcc
Gonpo Lama <gonpolama89@gmail.com>
2021-11-29 21:35:54 -0500

    Add logic for game, change computer selection to all lowercase

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 function computerPlay() {
-  let outcomes = ["Rock", "Paper", "Scissors"];
+  let outcomes = ["rock", "paper", "scissors"];
   let randomNum = Math.floor(Math.random() * 3);
 
   return outcomes[randomNum];
 }
 
 function playRound(playerSelection, computerSelection) {
+  let playerLower = playerSelection.toLowerCase();
 
+  if (playerLower === computerSelection) {
+    return "Try Again!";
+  }
+  else if (playerLower === "rock" && computerSelection === "scissors") {
+    return "You Win! Rock beats Paper";
+  } else if (playerLower === "paper" && computerSelection === "rock") {
+    return "You Win! Paper beats Rock";
+  } else if (playerLower === "scissors" && computerSelection === "paper") {
+    return "You Win! Scissors beats Paper";
+  } else if (playerLower === "rock" && computerSelection === "paper") {
+    return "You Lose! Paper beats Rock";
+  } else if (playerLower === "paper" && computerSelection === "scissors") {
+    return "You Lose! Scissors beats Paper";
+  } else {
+    return "You Lose! Rock beats Scissors";
+  }
 }
+
+const playerSelection = "Rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
