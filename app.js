6abaf8e40e08b56ee1f722b489bf105ca49293b3
Gonpo Lama <gonpolama89@gmail.com>
2021-11-29 21:13:45 -0500

    Create function that randomly returns either rock, paper, or scissors

---
+++ app.js
@@ -0,0 +1,10 @@
+function computerPlay() {
+  let outcomes = ["Rock", "Paper", "Scissors"];
+  let randomNum = Math.floor(Math.random() * 3);
+
+  return outcomes[randomNum];
+}
+
+function playRound(playerSelection, computerSelection) {
+
+}
