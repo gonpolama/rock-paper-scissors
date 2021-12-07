f1bd8477f76d530d42ac44dc1bf58a33624004ee
Gonpo Lama <gonpolama89@gmail.com>
2021-12-07 01:17:37 -0500

    Display the running score, and announce a winner of the game once one player reaches 5 points.

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ let buttons = Array.from(document.querySelectorAll('button'));
 let rock = document.querySelector(".rock");
 let paper = document.querySelector(".paper");
 let scissors = document.querySelector(".scissors");
-let results = document.querySelector(".results")
+let results = document.querySelector(".results");
+let playScore = document.querySelector(".playScore");
+let compScore = document.querySelector(".compScore");
+
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
-  let outcomes = ["rock", "paper", "scissors"];
+  let outcomes = ["Rock", "Paper", "Scissors"];
   let randomNum = Math.floor(Math.random() * 3);
 
   return outcomes[randomNum];
@@ -15,58 +20,51 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     results.innerHTML = "<p>Try Again!</p>";
   }
-  else if (playerSelection === "rock" && computerSelection === "scissors") {
+  
+  else if( (playerSelection === "Rock" && computerSelection === "Scissors") ||
+           (playerSelection === "Paper" && computerSelection === "Rock") ||
+           (playerSelection === "Scissors" && computerSelection === "Paper")) {
     playerScore++;
-    results.innerHTML = "<p>You Win! Rock beats Paper</p>";
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    playerScore++;
-    results.innerHTML = "<p>You Win! Paper beats Rock</p>";
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    playerScore++;
-    results.innerHTML = "<p>You Win! Scissors beats Paper</p>";
-  } else if (playerSelection === "rock" && computerSelection === "paper") {
-    computerScore++;
-    results.innerHTML = "<p>You Lose! Paper beats Rock</p>";
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    computerScore++;
-    results.innerHTML = "<p>You Lose! Scissors beats Paper</p>";
-  } else {
-    computerScore++;
-    results.innerHTML = "<p>You Lose! Rock beats Scissors</p>";
+    results.innerHTML = `<p>You Win! ${playerSelection} beats ${computerSelection}</p>`;
+
+    if (playerScore === 5) {
+      alert("You win the game! Reload the page to play again.")
+    }
   }
+
+  else if ( (playerSelection === "Rock" && computerSelection === "Paper") ||
+            (playerSelection === "Paper" && computerSelection === "Scissors") ||
+            (playerSelection === "Scissors" && computerSelection === "Rock")) {
+    computerScore++;
+    results.innerHTML = `<p>You Lose! ${computerSelection} beats ${playerSelection}</p>`;
+
+    if (computerScore === 5) {
+      alert ("The computer won the game! Reload the page to play again.")
+    }
+  }
+
+  playScore.innerHTML = playerScore;
+  compScore.innerHTML = computerScore;
 }
 
 function random() {
-  if (this === rock) {
-    playRound("rock", computerPlay());
-  }
-  else if (this === paper) {
-    playRound("paper", computerPlay());
-  }
-  else {
-    playRound("scissors", computerPlay());
+  if (playerScore === 5) {
+    alert("Player Wins!");
+    return;
+  } else if (computerScore === 5) {
+    alert("Computer Wins!");
+    return;
   }
 
+  if (this === rock) {
+    playRound("Rock", computerPlay());
+  }
+  else if (this === paper) {
+    playRound("Paper", computerPlay());
+  }
+  else {
+    playRound("Scissors", computerPlay());
+  }
 }
 
 buttons.forEach(button => button.addEventListener('click', random));
-
-let playerScore = 0;
-let computerScore = 0;
-
-// function game() {
-//   for (let i=0; i<5; i++) {
-//     playRound(prompt("Rock, Paper, or Scissors?"), computerPlay());
-//     console.log()
-//   }
-//
-//   if (playerScore > computerScore) {
-//     console.log(`You Win! ${playerScore} - ${computerScore}`);
-//   } else if (computerScore > playerScore) {
-//     console.log(`You Lose! ${computerScore} - ${playerScore}`);
-//   } else {
-//     console.log(`It's a tie! ${computerScore} - ${playerScore}`);
-//   }
-// }
-
-// game();
